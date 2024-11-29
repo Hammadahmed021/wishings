2ab1060f8c939078823df68f87b4788baab87c10
Muhammad Saimoon <64534676+Saimoon0311@users.noreply.github.com>
2024-11-29 21:17:25 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-export {default as Home} from './Home.jsx'
-export {default as Pages} from './Pages.jsx'
-export {default as TemplatePage} from './TemplatePage.jsx'
-export {default as SignIn} from './SignIn.jsx'
-export {default as SignUp} from './SignUp.jsx'
+export { default as Home } from "./Home.jsx";
+export { default as Pages } from "./Pages.jsx";
+export { default as TemplatePage } from "./TemplatePage.jsx";
+export { default as SignIn } from "./SignIn.jsx";
+export { default as SignUp } from "./SignUp.jsx";
 export { default as Profile } from "./Profile.jsx";
 export { default as CheckOutPage } from "./CheckOutPage.jsx";
 export { default as AllOrdersScreen } from "./AllOrderPae.jsx";
-
+export { default as OrderDetail } from "./OrderDetail.jsx";
